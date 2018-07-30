a3f6ef22b01b7950e7d478e45771a107fdb5641e
Pahlorus <katyshev1980@yandex.ru>
2018-07-30 16:55:13 +0200

    class Card

--- Kozel_unity/Assets/ScriptsCore/Card.cs
+++ Kozel_unity/Assets/ScriptsCore/Card.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 namespace GameCore
 {
+
+
+
     public class Card
     {
+        #region Поля
+        private Suits _suit;
+        private Values _value;
+        #endregion
 
+        #region Свойства
+        public Suits Suit
+        {
+            get { return _suit; }
+            set { _suit = value; }
+        }
+        public Values Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+        public string Name 
+        {
+            get { return _value.ToString() + "_of_" + _suit.ToString(); }
+        }
+        #endregion
+
+        public Card(Suits suit, Values value)
+        {
+            _suit = suit;
+            _value = value;
+        }
     }
 }
