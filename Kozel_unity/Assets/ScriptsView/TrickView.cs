8356b246e3564e9eda610b5ca0b089b7362206e6
Pahlorus <katyshev1980@yandex.ru>
2018-07-30 12:54:21 +0200

    First commit
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Kozel_unity/Assets/ScriptsView/TrickView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameView
+{
+    public class TrickView : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
