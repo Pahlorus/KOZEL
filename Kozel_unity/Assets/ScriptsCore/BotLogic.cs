683cb3d93775cf85985527fd610ddfdd183f76b0
Pahlorus <katyshev1980@yandex.ru>
2018-08-03 23:18:21 +0200

    Correct
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Kozel_unity/Assets/ScriptsCore/BotLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameCore
+{
+   public  class BotLogic
+    {
+        public Card BotSelectCard(List<Card> cardsOnHands, Card[] arrayCardOnTable)
+        {
+            Card card = new Card();
+            return card;
+        }
+    }
+}
---
+++ Kozel_unity/Assets/ScriptsCore/CardDesign.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameCore
+{
+    public enum Suits
+    {
+        Clubs,
+        Spades,
+        Hearts,
+        Diamonds
+    }
+    public enum Values
+    {
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        King,
+        Ten,
+        Ace,
+        Jack,
+        Queen
+    }
+    //Переделать козыри в массив!
+    public enum Trumps
+    {
+        Six_of_Clubs,
+        Queen_of_Clubs,
+        Queen_of_Spades,
+        Queen_of_Hearts,
+        Queen_of_Diamonds,
+        Jack_of_Clubs,
+        Jack_of_Spades,
+        Jack_of_Hearts,
+        Jack_of_Diamonds,
+        Ace_of_Diamonds,
+        Ten_of_Diamonds,
+        King_of_Diamonds,
+        Nine_of_Diamonds,
+        Eight_of_Diamonds,
+        Seven_of_Diamonds,
+        Six_of_Diamonds
+    }
+
+
+
+}
